812f53b3f269c7fbbf137059222e8acb1401588a
Erkan Bostan <profesyonel59@gmail.com>
2022-03-10 05:07:44 +0300

    JPGToPNG
    JPG Dosyalarının PNG Dosyalarına dönüşümünü sağlayan program

---
+++ JPG To PNG Kodlar/JPG To PNG Kodlar/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace JPG_To_PNG_Kodlar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.OPF = new System.Windows.Forms.OpenFileDialog();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("simpleButton1.ImageOptions.SvgImage")));
+            this.simpleButton1.Location = new System.Drawing.Point(41, 98);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(150, 58);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Dosya Seç !";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "JPG To PNG !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 200);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.simpleButton1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JPG To PNG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.OpenFileDialog OPF;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JPG To PNG Kodlar/JPG To PNG Kodlar/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace JPG_To_PNG_Kodlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            OPF.ShowDialog();
+            FBD.ShowDialog();
+            Image bmpImageToConvert = Image.FromFile(OPF.FileName);
+            Image bmpNewImage = new Bitmap(bmpImageToConvert.Width,
+                                           bmpImageToConvert.Height);
+            Graphics gfxNewImage = Graphics.FromImage(bmpNewImage);
+            gfxNewImage.DrawImage(bmpImageToConvert,
+                                  new Rectangle(0, 0, bmpNewImage.Width,
+                                                bmpNewImage.Height),
+                                  0, 0,
+                                  bmpImageToConvert.
+                                  Width, bmpImageToConvert.Height,
+                                  GraphicsUnit.Pixel);
+            gfxNewImage.Dispose();
+            bmpImageToConvert.Dispose();
+            bmpNewImage.Save(FBD.SelectedPath + "\\Resim.png", ImageFormat.Png);
+
+            XtraMessageBox.Show("JPG Dosyası PNG Dosyasına Başarıyla Dönüştürüldü..!", "GeoTex Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
